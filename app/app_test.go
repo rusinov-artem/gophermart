d26d787185e286d167390f2944305d201346a718
Artem Rusinov <a.rusinov@lamoda.ru>
2024-08-03 12:28:22 +0300

    001 can run unit tests

---
+++ app/app_test.go
@@ -0,0 +1,11 @@
+package app
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Nothing(t *testing.T) {
+	assert.False(t, true)
+}
--- test/test.bash
+++ test/test.bash
@@ -11,4 +11,30 @@ fi
 
 echo "OK! Compilation succeeded"
 
+echo "Runing tests..."
+
+TEST_CMD="go test -v -count=1 -json -coverpkg=./... -coverprofile=coverage.out ./..."
+
+TEST_OUT=$(${TEST_CMD})
+
+R_VAL=$?
+if [[ R_VAL -ne "0" ]] ; then
+  FAIL="TRUE"
+fi
+
+# Filter failed tests
+ FAILED_TESTS=$( echo "${TEST_OUT}" | jq -c 'select(.Action=="fail")')
+ R_VAL=$?
+ if [[ R_VAL -ne "0" ]] ; then
+   echo "!!! TESTS FAILED !!!"
+   exit 2
+ fi
+
+if [[ ${FAIL} ]]; then
+    echo "${FAILED_TESTS}"
+    echo "!!! TESTS FAILED !!!"
+    exit 3 # needed to make build red
+else
+   echo "!!! TESTS PASSED !!!"
+fi
 
