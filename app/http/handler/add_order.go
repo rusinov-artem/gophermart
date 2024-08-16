7995db91124126ec2723a1b677bf17ee549edce7
Artem Rusinov <a.rusinov@lamoda.ru>
2024-08-16 11:28:06 +0300

    014 extracted getToken

--- app/http/handler/add_order.go
+++ app/http/handler/add_order.go
@@ -52,16 +52,3 @@ func (h *Handler) AddOrder(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusAccepted)
 }
-
-func getToken(r *http.Request) string {
-	if v := r.Header.Get("Authorization"); v != "" {
-		return v
-	}
-
-	cookie, err := r.Cookie("Authorization")
-	if err != nil {
-		return ""
-	}
-
-	return cookie.Value
-}
